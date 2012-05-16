853710702f997f8f2e1de9589581390cafffca65
David Grudl <david@grudl.com>
2012-05-16 19:58:08 +0200

    updated ApiGen to 2.6.1

--- tools/ApiGen/libs/TokenReflection/TokenReflection/ReflectionAnnotation.php
+++ tools/ApiGen/libs/TokenReflection/TokenReflection/ReflectionAnnotation.php
@@ -2,7 +2,7 @@
 /**
  * PHP Token Reflection
  *
- * Version 1.2
+ * Version 1.2.2
  *
  * LICENSE
  *
@@ -204,7 +204,7 @@ class ReflectionAnnotation
 				}
 
 				// @annotation
-				if (preg_match('~^@([\\S]+)\\s*(.*)~', $line, $matches)) {
+				if (preg_match('~^\\s*@([\\S]+)\\s*(.*)~', $line, $matches)) {
 					$name = $matches[1];
 					$this->annotations[$name][] = $matches[2];
 					continue;
